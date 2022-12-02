698d7320ab2a929cf98c595a406c66555269db08
Joel <joel@gluth.io>
2022-12-02 20:39:11 +0000

    Day 2

--- src/aoc_2022/day_2.clj
+++ src/aoc_2022/day_2.clj
@@ -1,2 +1,56 @@
 (ns aoc-2022.day-2)
 
+
+(defn shape-score [shape]
+  (case shape
+    :rock 1
+    :paper 2
+    :scissors 3
+    0))
+
+
+(def your-shape-codes
+  {"A" :rock
+   "B" :paper
+   "C" :scissors})
+
+(def my-shape-codes
+  {"X" :rock
+   "Y" :paper
+   "Z" :scissors})
+
+;; shape [defeats loses-to]
+(def shape-rules
+  {:rock [:scissors :paper]
+   :scissors [:paper :rock]
+   :paper [:rock :scissors]})
+
+
+(defn result-score [your-shape my-shape]
+  (let [[win-shape lose-shape] (get shape-rules my-shape)]
+    (cond
+      (= win-shape your-shape) 6
+      (= lose-shape your-shape) 0
+      (= your-shape my-shape) 3)))
+
+
+(defn round-score [your-shape my-shape]
+  (+ (shape-score my-shape)
+     (result-score your-shape my-shape)))
+
+
+(defn round-score-predetermined [your-shape-enc my-shape-enc]
+  (let [my-shape (get my-shape-codes my-shape-enc)]
+    (round-score (get your-shape-codes your-shape-enc)
+                 my-shape)))
+
+
+(defn round-score-deliberate [your-shape-enc my-outcome-enc]
+  (let [your-shape (get your-shape-codes your-shape-enc)
+        [lose-shape win-shape] (get shape-rules your-shape)
+        my-shape (case my-outcome-enc
+                   "X" lose-shape
+                   "Y" your-shape
+                   "Z" win-shape)]    
+    (round-score your-shape my-shape)))
+
--- test/aoc_2022/day_2_test.clj
+++ test/aoc_2022/day_2_test.clj
@@ -1,7 +1,49 @@
 (ns aoc-2022.day-2-test
   (:require [clojure.test :refer :all]
-            [aoc-2022.day-2 :refer :all]))
+            [aoc-2022.day-2 :refer :all]
+            [clojure.string :as str]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest shape-scoring-test
+  (testing "Shapes should be scored correctly"
+    (is (= (shape-score :rock) 1))
+    (is (= (shape-score :paper) 2))
+    (is (= (shape-score :scissors) 3))
+    (is (= (shape-score :nothing) 0))))
+
+(deftest result-scoring-test
+  (testing "Appropriate scores for win, lose or draw"
+    (is (= (result-score :rock :paper) 6))
+    (is (= (result-score :paper :rock) 0)
+        (= (result-score :scissors :scissors) 3))))
+
+(deftest round-scoring-with-encoded-choice-test
+  (testing "Rounds should be scored correctly when the encoding represents a specific move"
+    (is (= (round-score-predetermined "A" "Y") 8))
+    (is (= (round-score-predetermined "B" "X") 1))
+    (is (= (round-score-predetermined "C" "Z") 6))))
+
+
+(deftest round-scoring-with-deliberate-outcome-test
+  (testing "Rounds should be scored correctly when the outcome is predetermined"
+    (is (= (round-score-deliberate "A" "Y") 4))
+    (is (= (round-score-deliberate "B" "X") 1)
+        (= (round-score-deliberate "C" "Z") 7))))
+
+;; problems
+
+(def test-data
+  (with-open [rdr (clojure.java.io/reader "input-2.txt")]
+    (into [] (map #(str/split % #" ") (line-seq rdr)))))
+
+(deftest part-1-test
+  (testing "The total should be correct when using the predetermined version of the choice encoding"
+    (is (= (reduce + (map (partial apply round-score-predetermined)
+                          test-data))
+           11150))))
+
+(deftest part-1-test
+  (testing "The total should be correct when using the predetermined version of the choice encoding"
+    (is (= (reduce + (map (partial apply round-score-deliberate)
+                          test-data))
+           8295))))
+
