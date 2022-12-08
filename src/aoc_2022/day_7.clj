7c943287ca066a66a57b1f14ef8d5bc8fc2448a2
Joel Gluth <joel@gluth.io>
2022-12-08 16:34:01 +0000

    Day 7

--- src/aoc_2022/day_7.clj
+++ src/aoc_2022/day_7.clj
@@ -3,5 +3,57 @@
             [clojure.string :as str]))
 
 
-(defn split-on-spaces [astr]
-  #(str/split astr #" "))
+(defn split-on-spaces [aseq]
+  (map #(str/split % #" ") aseq))
+
+(defn ls [output-lines]
+  (into {}
+        (map (fn [[info filename]]
+               (if (= "dir" info)
+                 [filename {}]
+                 [filename (Integer/parseInt info)])) output-lines)))
+
+
+(defn cd [path-stack arg]
+  (case arg
+    ".." (pop path-stack)
+    "/" '()
+    (conj path-stack arg)))
+
+(defn process-command [path-stack dir-tree command-seq]
+  (let [[[command & args] & output] command-seq]
+    (case command
+      "cd"
+      [(cd path-stack (first args)) dir-tree]
+      "ls"
+      [path-stack
+       (if (empty? path-stack) ;; this is ugly but assoc-in is weird
+         (ls output)
+         (assoc-in dir-tree (reverse path-stack) (ls output)))])))
+
+
+(defn process-commands
+  ([command-seq]
+   (process-commands '() {} command-seq))
+  ([path-stack dir-tree command-seq]
+   (if (empty? command-seq)
+     [path-stack dir-tree]
+     (let [[path' dir'] (process-command path-stack dir-tree (first command-seq))]
+       (recur path' dir' (rest command-seq))))))
+
+(defn directory-size [directory-map]
+  (reduce + (map (fn [[name info]]
+                   (if (map? info)
+                     (directory-size info)
+                     info))
+                 (seq directory-map))))
+
+(defn find-all-dirs
+  ([dir-tree]
+   (find-all-dirs [] dir-tree))
+  ([found-dirs dir-tree]
+   (let [subdirs (map second (filter (fn [[name thing]] (map? thing)) (seq dir-tree)))]
+     
+     (conj 
+      (reduce concat found-dirs (map find-all-dirs subdirs))
+      dir-tree))))
--- test/aoc_2022/day_7_test.clj
+++ test/aoc_2022/day_7_test.clj
@@ -1,5 +1,6 @@
 (ns aoc-2022.day-7-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.string :as str]
+            [clojure.test :refer :all]
             [aoc-2022.core :refer :all]
             [aoc-2022.day-7 :refer :all]))
 
@@ -31,11 +32,112 @@ $ ls
 
 ;; each element is now a vector whose first element is a command string, and whose following elements are the output lines
 (def toy-commands
-  (map #(str/split % #"\n")
-       (map str/trim
-            (rest (str/split toy-data #"\$")))))
+  (map split-on-spaces
+       (map #(str/split % #"\n")
+            (map str/trim
+                 (rest (str/split toy-data #"\$"))))))
 
-(deftest leaf-dir-test
+(deftest command-parsing-test
   (testing "From command output, a directory without subdirectories gets puplated correctly"
-    (is (= 0 1))))
+    (is (= '(["ls"] ["dir" "e"] ["29116" "f"] ["2557" "g"] ["62596" "h.lst"])
+           (nth toy-commands 3)))))
 
+(deftest directory-node-building-test
+  (testing "an ls command in a directory with no subdirectories builds the right map"
+    (is (= { "j"    4060174
+            "d.log" 8033020
+            "d.ext" 5626152
+            "k"     7214296}
+           (ls '(["4060174" "j"]
+                 ["8033020" "d.log"]
+                 ["5626152" "d.ext"]
+                 ["7214296" "k"])))))
+  
+  (testing "An ls command in a directory with subdirectories puts placeholder maps in for them"
+    (is (= {"a"     {}
+            "d"     {}
+            "b.txt" 14848514
+            "c.dat" 8504156}
+           (ls '(["dir" "a"]
+                 ["14848514" "b.txt"]
+                 ["8504156" "c.dat"]
+                 ["dir" "d"]))))))
+
+(deftest cd-test
+  (testing "Given a directory tree and a current working directory stack, cd into a subdirectory by adding a new entry to the stack"
+    (is (= '("b" "a")
+           (cd '("a") "b"))))
+  (testing "Changing directory upwards works as expected"
+    (is (= '("a")
+           (cd '("b" "a") ".."))))
+  (testing "Changing to the root directory works as expected"
+    (is (= '()
+           (cd '("b" "a") "/")))))
+
+
+(deftest command-procesing-test
+
+  (testing "Given a current directory and a path stack, a cd command correctly updates the path stack"
+    (is (= ['("b")
+            { "a" 10
+             "b" {}}]))
+    (process-command '() 
+                     { "a" 10
+                      "b" {}}
+                     [["cd" "b"]]))
+
+  (testing "Given a current directory and a path stack, an ls command updates the directory tree"
+    (is (= ['("b") { "a" 10
+                    "b" { "c" 20 }
+                    }]
+           (process-command '("b")
+                            { "a" 10
+                             "b" {}}
+                            [["ls"] ["20" "c"]]))))
+  
+  (testing "Given a current directory and path stack, a cd into a leaf node followed by an ls updates the path stack and directory tree"
+    (comment (is (= ['("b") { "a" 10
+                           "b" { "c" 20 }
+                           }]
+                  (process-commands '()
+                                    { "a" 10
+                                     "b" { "c" 20 }
+                                     }
+                                    [["cd" "b"]
+                                     ["ls" ["20" "c"]]]))))))
+
+(deftest directory-size-test
+  
+  (testing "Given a leaf node, the total size of the directory's files is returned"
+    (is (= 24933642 (directory-size {"j"    4060174
+                                     "d.log" 8033020 
+                                     "d.ext" 5626152 
+                                     "k"     7214296}))))
+  (testing "Given a nested directory, the total size of the directorys' files is returned"
+
+    (is (= 24933642 (directory-size {"j"    4060174
+                                     "d.log" 8033020 
+                                     "subdir" {"d.ext" 5626152 
+                                               "k"     7214296}}))))
+
+  (testing "Given the toy test data, the size is as reported in the problem statement"
+    (is (= 48381165 (directory-size (second (process-commands toy-commands)))))))
+
+;; Problems
+
+(def test-commands
+  
+  (map split-on-spaces
+       (map #(str/split % #"\n")
+            (map str/trim
+                 (rest (str/split (slurp "input-7.txt") #"\$"))))))
+
+(def test-tree (second (process-commands '() {} test-commands)))
+
+(deftest part-1-test
+  (is (= 1454188
+         (reduce + (filter #(< % 100000) (map directory-size (find-all-dirs test-tree)))))))
+
+(deftest part-2-test
+  (is (= 4183246
+         (first (sort (filter #(> % (- 30000000 (- 70000000 (directory-size test-tree)))) (map directory-size (find-all-dirs test-tree))))))))
