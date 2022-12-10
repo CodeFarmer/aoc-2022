68f38fc49564d698870103f07afe0ac9f120c00a
Joel Gluth <joel@gluth.io>
2022-12-10 11:09:09 +0000

    Skeleton for day 10, going back to the tablet. I may be some time.

---
+++ src/aoc_2022/day_10.clj
@@ -0,0 +1,4 @@
+(ns aoc-2022.day-10
+  (:require [aoc-2022.core :refer :all]))
+
+
---
+++ test/aoc_2022/day_10_test.clj
@@ -0,0 +1,11 @@
+(ns aoc-2022.day-10-test
+  (:require [clojure.test :refer :all]
+            [clojure.string :as str]
+            [aoc-2022.core :refer :all]
+            [aoc-2022.day-10 :refer :all]))
+
+(deftest a-test
+  (testing "something"
+    (is (= 1 0))))
+
+(def test-data (lines-as-vector "input-10.txt"))
