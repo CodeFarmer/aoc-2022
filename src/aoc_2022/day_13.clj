e5849925c963ea91fa9ce689f9f3e67bdff18a26
Joel <joel@gluth.io>
2022-12-13 07:22:04 +0000

    Day 13 skeleton in case I have time today

---
+++ src/aoc_2022/day_13.clj
@@ -0,0 +1,5 @@
+(ns aoc-2022.day-13
+  (:require [clojure.string :as str]
+            [aoc-2022.core :refer :all]))
+
+
---
+++ test/aoc_2022/day_13_test.clj
@@ -0,0 +1,9 @@
+(ns aoc-2022.day-13-test
+  (:require [clojure.test :refer :all]
+            [clojure.string :as str]
+            [aoc-2022.core :refer :all]
+            [aoc-2022.day-13 :refer :all]))
+
+(deftest a-test
+  (testing "something"
+    (is (= 0 1))))
