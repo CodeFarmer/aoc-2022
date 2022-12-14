e2644a06b5d79fd36ca363f81f766e67a97e981c
Joel Gluth <joel@gluth.io>
2022-12-14 23:10:14 +0000

    Day 14 part 1

--- src/aoc_2022/day_14.clj
+++ src/aoc_2022/day_14.clj
@@ -2,3 +2,111 @@
   (:require [clojure.string :as str]
             [aoc-2022.core :refer :all]))
 
+(defn parse-path [line]
+  (map #(intify-seq (str/split % #","))
+       (str/split line #" -> "))) 
+
+
+(defn cave-bounds
+  ([cave]
+   (cave-bounds [Integer/MAX_VALUE Integer/MAX_VALUE]
+                [Integer/MIN_VALUE Integer/MIN_VALUE]
+                cave))
+  ([[minx miny] [maxx maxy] cave]
+   (if (empty? cave)
+     [[minx miny] [maxx maxy]]
+     (let [[x y] (first cave)
+           minx' (min minx x)
+           miny' (min miny y)
+           maxx' (max maxx x)
+           maxy' (max maxy y)]
+       (recur [minx' miny'] [maxx' maxy'] (rest cave))))))
+
+(defn cave-bounds-with-sand
+  [cave sand]
+  (cave-bounds (into cave sand)))
+
+(defn line-between [[x1 y1] [x2 y2]]
+  (into #{}
+        (for [x (if (> x2 x1 )
+                  (range x1 (inc x2))
+                  (range x2 (inc x1)))
+              y (if (> y2 y1)
+                  (range y1 (inc y2))
+                  (range y2 (inc y1)))]
+          [x y])))
+
+(defn rock-path [cave path]
+  (reduce into cave (map (partial apply line-between) (partition 2 1 path))))
+
+(defn ctos
+  "cave to string. this is the product of a tired mind"
+  ([cave]
+   (ctos cave #{}))
+  ([cave sand]
+   (str/join "\n"
+             (map (partial apply str)
+                  (let [[[minx miny] [maxx maxy]] (cave-bounds-with-sand cave sand)
+                        width (inc (- maxx minx))
+                        height (inc (- maxy miny))]
+                    (reduce (fn [acc [x y c]]
+                              (assoc-in acc [(- y miny) (- x minx)] c))
+                            (apply vector (repeat height []))
+                            (for [x (range minx (inc maxx))
+                                  y (range miny (inc maxy))
+                                  :let [c (if (cave [x y])
+                                            \#
+                                            (if (sand [x y])
+                                              \o
+                                              \.))]]
+                              [x y c])))))))
+
+(defn all-rock-paths [cave paths]
+  (reduce rock-path cave paths))
+
+(defn tick
+  ([cave grain]
+   (tick cave #{} grain))
+  ([cave sand [grain-x grain-y]]
+   (let [straight-down [grain-x (inc grain-y)]
+         down-left [(dec grain-x) (inc grain-y)]
+         down-right [(inc grain-x) (inc grain-y)]]
+     (cond (not (or (cave straight-down)
+                    (sand straight-down)))
+           straight-down
+           (not (or (cave down-left)
+                    (sand down-left)))
+           down-left
+           (not (or (cave down-right)
+                    (sand down-right)))
+           down-right
+           :default [grain-x grain-y]))))
+
+(defn drop-sand [cave sand grain]
+  (let [[[minx miny] [maxx maxy]] (cave-bounds cave)
+        [x' y'] (tick cave sand grain)]
+    (cond (= grain [x' y']) ;; grain has stopped
+          [cave (conj sand grain)]
+          (or (< x' minx)
+              (> x' maxx)
+              (> y' maxy)) ;; fallen off the map
+          [cave sand]
+          :default (recur cave sand [x' y']))))
+
+
+(defn -drop-a-lot-of-sand
+  [cave sand starting-point]
+  (iterate (fn [[cave sand]] (drop-sand cave sand starting-point))
+           [cave sand]))
+
+(defn drop-a-lot-of-sand
+  [cave sand n starting-point]
+  (nth (-drop-a-lot-of-sand cave sand starting-point) n))
+
+(defn last-caught-sand
+  [cave sand starting-point]
+  (loop [index 0
+         state-pairs (partition 2 1 (-drop-a-lot-of-sand cave sand starting-point))]
+    (if (apply = (first state-pairs))
+      index
+      (recur (inc index) (rest state-pairs)))))
--- test/aoc_2022/day_14_test.clj
+++ test/aoc_2022/day_14_test.clj
@@ -5,6 +5,86 @@
             [aoc-2022.day-14 :refer :all]))
 
 
+(def toy-lines (str/split "498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9"
+                         #"\n"))
+
+(deftest parse-path-test
+  (testing "path is parsed correctly to sequences of points"
+    (is (= [[498 4] [498 6] [496 6]]
+           (parse-path (first toy-lines))))))
+
+(deftest line-between-test
+  (testing "a vertical line between the first two points is the set of all points between them inclusive"
+    (is (= #{[498 4] [498 5] [498 6]}
+           (line-between [498 4] [498 6]))))
+  (testing "a vertical line between the second two points"
+    (is (= #{[496 6] [497 6] [498 6]}
+           (line-between [498 6] [496 6]))))
+  (testing "a line between two points is the same whichever way the points are ordered"
+    (is (= (line-between [498 6] [496 6])
+           (line-between [496 6] [498 6])))))
+
+(def toy-paths (map parse-path toy-lines))
+
+(deftest multi-segment-line-test
+  (testing "a path on a single line of input, drawn on an empty cave, returns the expected rock squares"
+    (is (= #{[497 6] [498 5] [496 6] [498 6] [498 4]}
+           (rock-path #{} (first toy-paths))))))
+
+(def toy-cave (all-rock-paths #{} toy-paths))
+
+(deftest multi-multi-segment-test
+  (is (= "....#...##
+....#...#.
+..###...#.
+........#.
+........#.
+#########."
+         (ctos toy-cave))))
+
+(deftest moving-sand-test
+  (testing "sand will drop directly down one unit if it can"
+    (is (= [500 2]
+           (tick toy-cave [500 1]))))
+  (testing "sand will drop diagonally down-left one unit if it can't go straight down"
+    (is (= [499 2]
+           (tick #{[500 2]} [500 1]))))
+  (testing "sand will drop diagonally down-right one unit if it can't go straight down or down-left"
+    (is (= [501 2]
+           (tick #{[500 2] [499 2]} [500 1]))))
+  (testing "a grain which can do none of these things will stay where it is"
+    (is (= [500 1]
+           (tick #{[499 2] [500 2] [501 2]} [500 1])))))
+
+(def dropping-sand-test
+  (testing "dropping a single grain of sand into a cave works correctly"
+    (is (= "....#...##
+....#...#.
+..###...#.
+........#.
+......o.#.
+#########."
+           (apply ctos (drop-sand toy-cave #{} [500 1]))))))
+
+(deftest dropping-lots-of-sand-test
+  (testing "Dropping consecutive grains of sand builds up as expected"(is (= "......o...
+.....ooo..
+....#ooo##
+....#ooo#.
+..###ooo#.
+....oooo#.
+...ooooo#.
+#########."
+                                                                             (apply ctos (drop-a-lot-of-sand toy-cave #{} 22 [500 1]))))))
+
+(deftest steady-state-detection-test
+  (testing "After 24 drops, no more sand accumulates"
+    (is (= 24 (last-caught-sand toy-cave #{} [500 1])))))
+
+;; problems
+
+(def test-cave (all-rock-paths #{} (map parse-path (lines-as-vector "input-14.txt"))))
+
 (deftest part-1-test
-  (testing "something"
-    (is (= 1 0))))
+  (is (= 805 (last-caught-sand test-cave #{} [500 1]))))
