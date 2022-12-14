a4c15d1080876b67150cb9558758e8e69efee07d
Joel <joel@gluth.io>
2022-12-14 16:44:58 +0000

    Day 14 skeleton, changing laptops again

---
+++ src/aoc_2022/day_14.clj
@@ -0,0 +1,4 @@
+(ns aoc-2022.day-14
+  (:require [clojure.string :as str]
+            [aoc-2022.core :refer :all]))
+
---
+++ test/aoc_2022/day_14_test.clj
@@ -0,0 +1,10 @@
+(ns aoc-2022.day-14-test
+  (:require [clojure.test :refer :all]
+            [clojure.string :as str]
+            [aoc-2022.core :refer :all]
+            [aoc-2022.day-14 :refer :all]))
+
+
+(deftest part-1-test
+  (testing "something"
+    (is (= 1 0))))
